15168858f55cd1c992ac0709fa585c064b9f8e4d
Florian Larguier <flo-du-30800@hotmail.fr>
2022-11-17 15:21:26 +0100

    Modification des pages Galerie + contact

--- script.js
+++ script.js
@@ -3,3 +3,41 @@ $(document).ready(function(){
     $("#Hs").slideToggle(500);
     });
     });
+
+
+    function valider(){
+        if(document.contactf.subject.value != "") {
+   
+          return true;
+        }
+        else {
+          alert("Veuillez saisir un nom de sujet");
+          return false;
+        }
+        
+      }
+
+      function valider(){
+        if(document.contactf.mail.value != "") {
+   
+          return true;
+        }
+        else {
+          alert("Veuilez saisir votre email");
+          return false;
+        }
+        
+      }
+
+      function valider(){
+        if(document.contactf.msg.value != "") {
+   
+          return true;
+        }
+        else {
+          alert("Veuilez écrire un message avant de valider");
+          return false;
+        }
+        
+      }
+      
